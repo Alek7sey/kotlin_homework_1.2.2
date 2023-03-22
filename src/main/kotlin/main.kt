5be608afed793c5fef5e6506a38cbdfb05368954
Alexey Ashikhmin <ash60782@gmail.com>
2023-03-22 22:52:20 +0500

    refinement of the algorithm

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,22 @@
+import kotlin.math.pow
+
 fun main() {
     identifyPeople(1)
-    identifyPeople(2)
     identifyPeople(7)
-    identifyPeople(10)
     identifyPeople(11)
     identifyPeople(32)
     identifyPeople(61)
+    identifyPeople(111)
+    identifyPeople(121)
+    identifyPeople(1011)
+    identifyPeople(10011)
+    identifyPeople(10021)
+    identifyPeople(100062)
 }
 
 fun identifyPeople(likes: Int) {
-    if (likes == 1 || (likes > 11 && likes % 10 == 1)) {
+    val length = likes.toString().length
+    if (likes != 11 && (likes % 10.toFloat().pow((length - 1)).toInt()) != 11 && likes % 10 == 1) {
         println("Понравилось $likes человеку")
     } else {
         println("Понравилось $likes людям")
